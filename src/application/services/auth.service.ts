a3d4fa5271f1b5d0de8906f228351c25f9c93748
Vinícius Souza <vine.vsb3@gmail.com>
2025-07-11 15:31:14 -0300

    feat(guard): add guard, add url parser initial structure

--- src/application/services/auth.service.ts
+++ src/application/services/auth.service.ts
@@ -1,21 +1,31 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UsersService } from "@application/services/users.service"
-import { ErrorMessages } from '@shared/utils/error-messages'
-import { AppError } from '@shared/utils/app-errors'
+import { UsersService } from '@application/services/users.service'
+import { Injectable } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { AppError } from '@shared/utils/app-errors'
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService, private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-    async signIn(username: string, pass: string): Promise<{ access_token: string }> {
-      const user = await this.usersService.checkAuth(username, pass)
-      if (!user) {
-        throw AppError.invalidCredentials()
-      }
-      const payload = { sub: user.id, username: user.email };
-      return {
-        access_token: await this.jwtService.signAsync(payload, { secret: process.env.JWT_SECRET, expiresIn: '2m' }), // fix set configuration in module and set expiration time
-      };
+  async signIn(
+    username: string,
+    pass: string,
+  ): Promise<{ access_token: string }> {
+    const user = await this.usersService.checkAuth(username, pass)
+    if (!user) {
+      throw AppError.invalidCredentials()
     }
+
+    const payload = { id: user.id, email: user.email }
+
+    return {
+      access_token: await this.jwtService.signAsync(payload, {
+        secret: process.env.JWT_SECRET,
+        expiresIn: '2m',
+      }), // fix set configuration in module and set expiration time
+    }
+  }
 }
---
+++ src/presentation/controllers/url-parser.controller.ts
@@ -0,0 +1,15 @@
+import { AuthOptionalGuard } from "@infrastructure/guards/auth-optional.guard";
+import { AuthGuard } from "@infrastructure/guards/auth.guard";
+import { Controller, Post, Body, UseGuards } from "@nestjs/common";
+import { UrlParserService } from "@presentation/services/url-parser.service";
+
+@Controller('url-parser')
+export class UrlParserController {
+  constructor(private readonly urlParserService: UrlParserService) {}
+
+  @Post()
+  @UseGuards(AuthOptionalGuard)
+  async parseUrl(@Body() url: string) {
+    return this.urlParserService.parseUrl(url, )
+  }
+}
