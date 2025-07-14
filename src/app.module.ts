8a9fcbc434c397a710a6fa8f123a297a4156648e
Vinícius Souza <vine.vsb3@gmail.com>
2025-07-13 23:16:01 -0300

    style(unnecessary imports): remove main unnecessary imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,10 @@
 import { Global, Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { ApplicationModule } from './modules/application.module'
 import { DatabaseModule } from './modules/database.module'
 import { DomainModule } from './modules/domain.module'
-import { ApplicationModule } from './modules/application.module'
 import { InfrastructureModule } from './modules/infrastructure.module'
-import { CreateUserUseCase } from '@application/use-cases/user/create-user-use-case'
-import { IUserRepository } from '@domain/ports/user.repository'
-import { GetAllUsersUseCase } from '@application/use-cases/user/get-all-users-use-case'
-import { GetUserUseCase } from '@application/use-cases/user/get-user-use-case'
-import { ValidateUserPasswordUseCase } from '@application/use-cases/user/validate-user-password-use-case'
 import { PresentationModule } from './modules/presentation.module'
-import { JwtModule } from '@nestjs/jwt'
 
 @Global()
 @Module({
